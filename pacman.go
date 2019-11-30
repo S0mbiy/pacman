30673893fd5da734806bbd4184ad8484f53cbc2c
fermtz99 <fernando.mtzt99@gmail.com>
2019-11-30 14:01:17 -0600

    ghost with print

--- pacman.go
+++ pacman.go
@@ -2,60 +2,75 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+  "time"
 )
 
-var pacmap [324]int
+var pacmap = [324]int{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+-1,1,1,1,1,1,1,1,-1,-1,1,1,1,1,1,1,1,-1,
+-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,
+-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,
+-1,1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,1,-1,
+-1,1,1,1,1,-1,1,1,-1,-1,1,1,-1,1,1,1,1,-1,
+-1,-1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,-1,-1,
+-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1,-1,-1,-1,-1,
+-1,-1,-1,-1,1,-1,1,-1,-3,-3,-1,1,-1,1,-1,-1,-1,-1,
+-1,-1,-1,-1,1,1,1,-1,-2,-2,-1,1,1,1,-1,-1,-1,-1,
+-1,1,1,1,1,-1,1,1,1,1,1,1,-1,1,1,1,1,-1,
+-1,1,-1,-1,1,-1,-1,-1,1,1,-1,-1,-1,1,-1,-1,1,-1,
+-1,1,1,-1,1,1,1,1,1,1,1,1,1,1,-1,1,1,-1,
+-1,-1,1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,1,-1,-1,
+-1,1,1,1,1,-1,1,1,-1,-1,1,1,-1,1,1,1,1,-1,
+-1,1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,
+-1,1,1,1,1,1,1,1,-1,-1,1,1,1,1,1,1,1,-1,
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
 var enemies [324]int
 
 func main() {
-	pacmap := [324]int{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-	-1,1,1,1,1,1,1,1,-1,-1,1,1,1,1,1,1,1,-1,
-	-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,
-	-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,
-	-1,1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,1,-1,
-	-1,1,1,1,1,-1,1,1,-1,-1,1,1,-1,1,1,1,1,-1,
-	-1,-1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,-1,-1,
-	-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1,-1,-1,-1,-1,
-	-1,-1,-1,-1,1,-1,1,-1,-3,-3,-1,1,-1,1,-1,-1,-1,-1,
-	-1,-1,-1,-1,1,1,1,-1,-2,-2,-1,1,1,1,-1,-1,-1,-1,
-	-1,1,1,1,1,-1,1,1,1,1,1,1,-1,1,1,1,1,-1,
-	-1,1,-1,-1,1,-1,-1,-1,1,1,-1,-1,-1,1,-1,-1,1,-1,
-	-1,1,1,-1,1,1,1,1,1,1,1,1,1,1,-1,1,1,-1,
-	-1,-1,1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,1,-1,-1,
-	-1,1,1,1,1,-1,1,1,-1,-1,1,1,-1,1,1,1,1,-1,
-	-1,1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,
-	-1,1,1,1,1,1,1,1,-1,-1,1,1,1,1,1,1,1,-1,
-	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
-	print(pacmap)
+  ch := make(chan string)
+
 	for i := 0; i < len(enemies); i++ {
     enemies[i] = 0
   }
+	for n := 0; n < 5; n++ {
+    fmt.Println("Running ghost ", n)
+    go ghost(n, ch)
+  }
+  <-ch
+
 }
 
-func print(pacmap [324]int){
+func print(pacmap, enemies [324]int){
+  fmt.Print("\033[2J")
 	for i := 0; i < 324; i++ {
 		if(i%18==0){
 			fmt.Println()
 		}
-		if(pacmap[i]==-1){
-			fmt.Print("██")
-		}else if(pacmap[i]==1){
-			fmt.Print("░░")
-		}else if(pacmap[i]==-2){
-			fmt.Print("▄▄")
-		}else if(pacmap[i]==-3){
-			fmt.Print("▀▀")
-		}
+    if(enemies[i]== 2) {
+      fmt.Print("ᗕᗒ")
+    }else{
+      if(pacmap[i]==-1){
+  			fmt.Print("██")
+  		}else if(pacmap[i]==1){
+  			fmt.Print("░░")
+  		}else if(pacmap[i]==-2){
+  			fmt.Print("▄▄")
+  		}else if(pacmap[i]==-3){
+  			fmt.Print("▀▀")
+  		}
+    }
 	}
 	fmt.Println()
+
 }
 
 func ghostUp(position int) (int) {
-  if position <= 17 {
+  if position < 18 {
     return ghostDown(position)
   }
   return position - 18
 }
+
 func ghostDown(position int) (int) {
   if position >= 306 {
     return ghostRight(position)
@@ -64,30 +79,32 @@ func ghostDown(position int) (int) {
 }
 
 func ghostRight(position int) (int) {
-  if position%17 == 0 {
+  if position%18 == 17 {
     return ghostLeft(position)
   }
   return position + 1
 }
 
 func ghostLeft(position int) (int) {
-  if position%17 == 1 || position == 0{
+  if position%18 == 0 {
     return ghostUp(position)
   }
   return position -1
 }
 
-func ghost(id int) {
-  position := 0
+func ghost(id int, cha chan string) {
+  position := 15
   lastPosition := position
-  fmt.Println(position)
+  // fmt.Println(position)
 
   // Change position random
   source := rand.NewSource(time.Now().UnixNano())
   generator := rand.New(source)
   for {
+    // fmt.Println("antes")
+    time.Sleep(time.Second/2)
     changePosition := generator.Intn(4)
-    fmt.Println(changePosition)
+    // fmt.Println(changePosition)
     switch changePosition {
     case 0:
       position = ghostUp(position)
@@ -101,8 +118,11 @@ func ghost(id int) {
     if pacmap[position] == -1 {
       position = lastPosition
     } else {
+      enemies[lastPosition] = 0
       lastPosition = position
-      fmt.Println("Actual position of ghost ", id, ": ", position)
+      enemies[position] = 2
+      print(pacmap, enemies)
+      // fmt.Println("Actual position of ghost ", id, ": ", position)
     }
   }
 
